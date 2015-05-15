a50ed6d6a670a842c5f70ac4c47d8aa99b5a404d
Nikolas Gamarra <nxgamarra@gmail.com>
2015-05-15 15:43:51 -0400

    did stuff

--- betaTest.c
+++ betaTest.c
@@ -20,7 +20,7 @@ int h ={88};            //the hour which is used in write595
 //the main method
 int main (void)
 {
-	printf ("\nSmartClock: betaTest\n"" --Nikolas Gamarra (NXG) 2015 \n""\n");
+	printf ("\nSmartClock: betaTest\n""\n --Nikolas Gamarra (NXG) 2015 \n""\n");
 
 	wiringPiSetup() ;
 
@@ -28,29 +28,32 @@ int main (void)
 	sr595Setup (100, 32, 0, 1, 2) ;
 
 	initializationTest();
-	
+
 	//makes program start at 0 seconds
 	int s = 1;
-        printf("\nWaiting until seconds = 0\n");
+	printf("\nWaiting until seconds = 0\n");
 
 	do
 	{
-	        time_t epoch_time;
-       		struct tm *tm_p;
-       		epoch_time = time( NULL );
-	        tm_p = localtime( &epoch_time );
+		time_t epoch_time;
+		struct tm *tm_p;
+		epoch_time = time( NULL );
+		tm_p = localtime( &epoch_time );
 
 		s = tm_p->tm_sec;
-        	printf("\rseconds = %d", s);
-                fflush(stdout);
+		printf("\rseconds = %d", s);
+		fflush(stdout);
 	}while(s != 0);
 
 	for(;;)
 	{
-	getTime();
-	write595();
-	printf("\nEvent: Display update complete! The clock will check the time in 60.006 seconds.\n");
-	delay(60006);
+		getTime();
+		for(int x=0;x <4; ++x)
+		{
+			writeDigit(x,input[x]);
+		}
+		printf("\nEvent: Display update complete! The clock will check the time in 60.006 seconds.\n");
+		delay(60006);
 	}
 
 	return 0;
@@ -76,8 +79,21 @@ void initializationTest()
 	printf("      GND |10     11| GND\n");
 	printf("          |---------|\n");
 
+	printf("\nUsing the TPIC6B595:\n");
+	printf("Connect Vcc to 5V\n");
+	printf("Connect the GND pins to the common GND\n");
+	printf("Connect Pin 3 to Data in pin on MCU\n");
+	printf("Connect Pin 13 to the Clock pin on the MCU\n");
+	printf("Connect Pin 12 to the Latch pin on the MCU\n");
+	printf("Connect Pin 18 to Pin 3 on the next REG\n");
+	printf("Tie Pin 8 to 5V\n");
+	printf("Tie Pin 9 to GND\n");
+
+	printf("\nPhysical connections:\n");
+	printf("The sClockProgram uses the GPIO pins [0, 1, 2] (Physical pins [11, 12, 13]) for Data, Clock, and Latch respectively. For the parallel connection, the green four connects to the first four registries with the black side connection to registry 1. The red four connections go to the next four registries with the black side on registry 4.\n");
+	delay(1500);
 	printf("\nBegining Reg Test\n");
-	
+
 	clear595();
 	digitalWrite (100, 1);
 	printf ("Testing Reg:0");
@@ -110,7 +126,7 @@ void getTime()
 	int hh = tm_p->tm_hour;
 	int m = tm_p->tm_min;
 	h = hh;
-	
+
 	//Changes to 12hr time
 	if (h >= 12)
 	{
@@ -164,6 +180,139 @@ void getTime()
 	printf("\n");
 }
 
+void writeSegment(int x)
+{
+	digitalWrite(x,1);
+}
+
+void writeDigit(int dig, int num)
+{
+
+	int shiftSize = dig * 8;
+
+	switch(num)
+	{
+		case 0:
+		/
+	if(digi==0)
+			{
+                        digitalWrite(100 + shiftSize,0);
+                        digitalWrite(101 + shiftSize,0);
+                        digitalWrite(102 + shiftSize,0);
+                        digitalWrite(103 + shiftSize,0);
+                        digitalWrite(104 + shiftSize,0);
+                        digitalWrite(105 + shiftSize,0);
+                        digitalWrite(106 + shiftSize,0);
+
+			}
+			if(dig !=0)
+			{
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,1);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,0);
+			}
+			break;
+
+		case 1:
+			digitalWrite(100 + shiftSize,0);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,0);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,0);
+			digitalWrite(106 + shiftSize,0);
+			break;
+
+		case 2:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,0);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,1);
+			digitalWrite(105 + shiftSize,0);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 3:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,0);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 4:
+			digitalWrite(100 + shiftSize,0);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,0);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 5:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,0);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 6:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,0);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,1);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 7:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,0);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,0);
+			digitalWrite(106 + shiftSize,0);
+			break;
+
+		case 8:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,1);
+			digitalWrite(104 + shiftSize,1);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		case 9:
+			digitalWrite(100 + shiftSize,1);
+			digitalWrite(101 + shiftSize,1);
+			digitalWrite(102 + shiftSize,1);
+			digitalWrite(103 + shiftSize,0);
+			digitalWrite(104 + shiftSize,0);
+			digitalWrite(105 + shiftSize,1);
+			digitalWrite(106 + shiftSize,1);
+			break;
+
+		default:
+			printf("This should not happen\n");
+			break;
+	}
+}
+
 void write595()
 {
 	printf ("\nEvent: write595 has been called\n");
@@ -173,10 +322,10 @@ void write595()
 	int toWrite = 0;      //the value of the digit	
 
 	clear595();
-	
+
 	//adds :
-	digitalWrite(107,1);
-	digitalWrite(115,1);
+	//digitalWrite(107,1);
+	//digitalWrite(115,1);
 
 	//skips dig0 when dig0=0
 	if(h < 10)
@@ -186,150 +335,153 @@ void write595()
 
 	for(dig; dig < 4; ++dig)
 	{
-                printf("Preforming operations on dig %d\n",dig);
+		printf("Preforming operations on dig %d\n",dig);
 
 		//sets size of shift for the specific dig
 		switch(dig)
 		{
-		case 0:
-       		shiftSize = 0;
-		break;
+			case 0:
+				shiftSize = 0;
+				break;
 
-                case 1:
-                shiftSize = 8;
-                break;
+			case 1:
+				shiftSize = 8;
+				break;
 
-                case 2:
-                shiftSize = 16;
-                break;
+			case 2:
+				shiftSize = 16;
+				break;
 
-                case 3:
-                shiftSize = 24;
-                break;
+			case 3:
+				shiftSize = 24;
+				break;
 
-                default:
-                printf("This should not happen\n");
-                break;
+			default:
+				printf("This should not happen\n");
+				break;
 
 		}
+		shiftSize = dig * 8;
 		//sets the value to be written
 		toWrite = input[dig];
-		
+
 		//writes to the 595, note that 100-106 are A-G respectively
 		switch(toWrite)
 		{
-                case 0:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,1);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,0);
-                break;
+			case 0:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,1);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,0);
 
-                case 1:
-	        digitalWrite(100 + shiftSize,0);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,0);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,0);
-                digitalWrite(106 + shiftSize,0);
-                break;
 
-                case 2:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,0);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,1);
-                digitalWrite(105 + shiftSize,0);
-                digitalWrite(106 + shiftSize,1);
-                break;
+				break;
 
-                case 3:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,0);
-                digitalWrite(106 + shiftSize,1);
-                break;
+			case 1:
+				digitalWrite(100 + shiftSize,0);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,0);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,0);
+				digitalWrite(106 + shiftSize,0);
+				break;
 
-                case 4:
-                digitalWrite(100 + shiftSize,0);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,0);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,1);
-                break;
+			case 2:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,0);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,1);
+				digitalWrite(105 + shiftSize,0);
+				digitalWrite(106 + shiftSize,1);
+				break;
 
-                case 5:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,0);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,1);
-                break;
+			case 3:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,0);
+				digitalWrite(106 + shiftSize,1);
+				break;
 
-                case 6:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,0);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,1);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,1);
-                break;
+			case 4:
+				digitalWrite(100 + shiftSize,0);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,0);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,1);
+				break;
 
-                case 7:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,0);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,0);
-                digitalWrite(106 + shiftSize,0);
-                break;
+			case 5:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,0);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,1);
+				break;
 
-                case 8:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,1);
-                digitalWrite(104 + shiftSize,1);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,1);
-                break;
+			case 6:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,0);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,1);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,1);
+				break;
 
-                case 9:
-                digitalWrite(100 + shiftSize,1);
-                digitalWrite(101 + shiftSize,1);
-                digitalWrite(102 + shiftSize,1);
-                digitalWrite(103 + shiftSize,0);
-                digitalWrite(104 + shiftSize,0);
-                digitalWrite(105 + shiftSize,1);
-                digitalWrite(106 + shiftSize,1);
-                break;
-		
-		default:
-		printf("This should not happen\n");
-		break;
+			case 7:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,0);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,0);
+				digitalWrite(106 + shiftSize,0);
+				break;
+
+			case 8:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,1);
+				digitalWrite(104 + shiftSize,1);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,1);
+				break;
+
+			case 9:
+				digitalWrite(100 + shiftSize,1);
+				digitalWrite(101 + shiftSize,1);
+				digitalWrite(102 + shiftSize,1);
+				digitalWrite(103 + shiftSize,0);
+				digitalWrite(104 + shiftSize,0);
+				digitalWrite(105 + shiftSize,1);
+				digitalWrite(106 + shiftSize,1);
+				break;
+
+			default:
+				printf("This should not happen\n");
+				break;
 		}
 	}
 }
 
 void clear595()
 {
-        for(int i = 0; i < 31; ++i)
-        {
-           //     digitalWrite (100 + i, 0);
-        }
+	for(int i = 0; i < 31; ++i)
+	{
+		//     digitalWrite (100 + i, 0);
+	}
 
 }
